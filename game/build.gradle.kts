cef56dd2aae869cd59beacc9c38d588b1ecfa43b
lauramarianella <lauramarianella@gmail.com>
2026-04-20 15:18:35 -0400

    Initial setup: JavaFX Gradle project with HelloApplication

---
+++ game/build.gradle.kts
@@ -0,0 +1,57 @@
+plugins {
+    java
+    application
+    id("org.javamodularity.moduleplugin") version "1.8.15"
+    id("org.openjfx.javafxplugin") version "0.0.13"
+    id("org.beryx.jlink") version "2.25.0"
+}
+
+group = "com.champlain.soft"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+val junitVersion = "5.12.1"
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(22)
+    }
+}
+
+tasks.withType<JavaCompile> {
+    options.encoding = "UTF-8"
+}
+
+application {
+    mainModule.set("com.champlain.soft.game")
+    mainClass.set("com.champlain.soft.game.HelloApplication")
+}
+
+javafx {
+    version = "22.0.1"
+    modules = listOf("javafx.controls", "javafx.fxml")
+}
+
+dependencies {
+    implementation("org.controlsfx:controlsfx:11.2.1")
+    implementation("com.dlsc.formsfx:formsfx-core:11.6.0") {
+      exclude(group = "org.openjfx")
+    }
+    testImplementation("org.junit.jupiter:junit-jupiter-api:${junitVersion}")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:${junitVersion}")
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
+
+jlink {
+    imageZip.set(layout.buildDirectory.file("/distributions/app-${javafx.platform.classifier}.zip"))
+    options.set(listOf("--strip-debug", "--compress", "2", "--no-header-files", "--no-man-pages"))
+    launcher {
+        name = "app"
+    }
+}
